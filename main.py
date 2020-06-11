01836f60146ab0362edf2bca2142c7ca9a82881a
Lauri Elias <laurileet@gmail.com>
2020-06-11 11:10:27 +0300

    Replace data

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ START_DATE = datetime.date(2001, 1, 1)
 END_DATE = datetime.date(2020, 6, 10)
 EMA_PERIOD = 50
 
-dataframe = pd.read_csv('sp500_2000.csv')
+dataframe = pd.read_csv('SPY.csv')
 volume_ema = dataframe.loc[:, ['Volume']].ewm(span=200).mean()
 price_ema = dataframe.loc[:, ['Close']].ewm(span=EMA_PERIOD).mean()
 
@@ -15,7 +15,7 @@ for row in dataframe.iloc[252:].itertuples():
         if price_ema.Close[row.Index] > row.Close:
             # Volume is 7th column
             volume_avg = dataframe.iloc[row.Index - EMA_PERIOD:row.Index, [6]].mean()
-            if volume_avg.Volume * 1.5 < row.Volume:
+            if volume_avg.Volume * 2 < row.Volume:
                 print(f'{row.Date} is a red day')
                 print(f'Price below EMA {row.Close} < {price_ema.Close[row.Index]}')
                 print(f'Volume spike detected, buy here')
